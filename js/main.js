d45fa705d7d94e9bdf74ff9e2fc25b6ab8d1047e
Sean Reilly <seanreilly123@hotmail.com>
2019-08-28 15:22:23 +1200

    Working on printing cart

--- js/main.js
+++ js/main.js
@@ -101,6 +101,88 @@ let dinner = 	[
 }
 ];
 
+let allMenuItems = 	[
+{
+	name:"French Toast",
+	description: "French Toast with blueberries",
+	price: 7
+},
+{
+	name:"Beans on Toast",
+	description: "Beans on Toast with beans",
+	price: 6
+},
+{
+	name:"Eggs Benedict",
+	description: "Eggs Benedict and Hollandaise Sauce",
+	price: 9.5
+},
+{
+	name:"Pancakes",
+	description: "Stack of pancakes with maple syrup and banana",
+	price: 8
+},
+{
+	name:"Eggs",
+	description: "Two eggs any style with any side",
+	price: 9
+},
+{
+	name:"5-Cheese Omelet",
+	description: "Omelet made with 5 cheeses and any side",
+	price: 10
+},
+{
+	name:"Nachos",
+	description: "Nachos with mince and beans and stuff",
+	price: 8.5
+},
+{
+	name:"Sandwich",
+	description: "Sandwich with your choice of 3 fillings",
+	price: 7
+},
+{
+	name:"Stew",
+	description: "Fresh stew made by Grandma",
+	price: 8
+},
+{
+	name:"Chips",
+	description: "Crispy chips with a side of aioli",
+	price: 4
+},
+{
+	name:"Wedges",
+	description: "Crispy wedges with a side of BBQ sauce",
+	price: 4.5
+},
+{
+	name:"Tacos",
+	description: "Fresh chicken or beef tacos with your choice of toppings",
+	price: 6
+},
+{
+	name:"Beef Burger",
+	description: "Succulent beef pattie topped with fresh lettuce and bacon",
+	price: 11
+},
+{
+	name:"Chicken Burger",
+	description: "Juicy chicken breast with lettuce and camembert",
+	price: 12
+},
+{
+	name:"Ribeye Steak",
+	description: "Succulent ribeye steak with garlic butter",
+	price: 24
+},
+{
+	name:"Salmon",
+	description: "Fresh King Salmon fron Nelson",
+	price: 21
+}
+];
 var output = document.getElementById("menu-output");
 var brek = document.getElementById("brek");
 var lun = document.getElementById("lunch");
@@ -109,6 +191,8 @@ var brekkie = document.getElementById("brekkie");
 var lunchie = document.getElementById("lunchie");
 var dinnie = document.getElementById("dinnie");
 var btn = "";
+var items = [];
+var cart = [];
 
 
 // event listeners for buttons to print right menu
@@ -170,10 +254,22 @@ function listMenu(menu) {
 		menuList.push("<h1>Dinner</h1>");
 	}
 	for (let i=0; i<menu.length; i++) {
-		menuList.push("<div class="+"thisClass"+"><h2>" + menu[i].name + "</h2><p>" + menu[i].description + "</p><p>$" + menu[i].price + "</p><button onclick="+"addToCart()"+">Add To Cart</button></div>");
+		menuList.push("<div class='menu-js'><h2>" + menu[i].name + "</h2><div class='menu-item-center'><p>" + menu[i].description + "</p><p>$" + menu[i].price + "</p></div><button class='addToCartBtn'>Add To Cart</button></div>");
 	}
 	menuList = menuList.join("");
 	output.innerHTML = menuList;
+
+	$(".addToCartBtn").click(function(e) {
+		items.push(e.target.parentNode.children['0'].innerText);
+		printMenuItem(items);
+	});
+
+}
+
+function printMenuItem(items){
+	for (var i = 0; i < items.length; i++) {
+		console.log(allMenuItems.find(x => x.name === items[i]));
+	}
 }
 
 function addToCart(){
